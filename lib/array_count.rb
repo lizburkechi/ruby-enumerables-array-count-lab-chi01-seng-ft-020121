03066232c900130945b531c26f45fb6fce2abf52
Liz Burke <liz.burke.chi@gmail.com>
2021-01-16 18:16:17 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -7,7 +7,7 @@ end
 end
 # Return the total number of EMPTY strings in the provided array using the count enumerable
 def count_empty_strings(array)
-  array.count do |num|
-    num.empty?
+  array.count do |element|
+    element == ""
 end
 end
